13df9bd7cec25884328cd9f7cb3b4830a9797033
Preston Richey <prichey@barkleyus.com>
2016-10-18 11:42:18 -0500

    Add working sound example

--- soundtest.py
+++ soundtest.py
@@ -1,43 +1,7 @@
-''' pg_playmp3f.py
-play MP3 music files using Python module pygame
-pygame is free from: http://www.pygame.org
-(does not create a GUI frame in this case)
-'''
+import subprocess, time
 
-import pygame as pg
+for i in range(3):
+    subprocess.call(['afplay', "assets/shortbeep.wav"])
+    time.sleep(1)
 
-def play_music(music_file, volume=0.8):
-    '''
-    stream music with mixer.music module in a blocking manner
-    this will stream the sound from disk while playing
-    '''
-    # set up the mixer
-    freq = 44100     # audio CD quality
-    bitsize = -16    # unsigned 16 bit
-    channels = 2     # 1 is mono, 2 is stereo
-    buffer = 2048    # number of samples (experiment to get best sound)
-    pg.mixer.init(freq, bitsize, channels, buffer)
-    # volume value 0.0 to 1.0
-    pg.mixer.music.set_volume(volume)
-    clock = pg.time.Clock()
-    try:
-        pg.mixer.music.load(music_file)
-        print("Music file {} loaded!".format(music_file))
-    except pg.error:
-        print("File {} not found! ({})".format(music_file, pg.get_error()))
-        return
-    pg.mixer.music.play()
-    while pg.mixer.music.get_busy():
-        # check if playback has finished
-        clock.tick(30)
-
-
-# pick a MP3 music file you have in the working folder
-# otherwise give the full file path
-# (try other sound file formats too)
-music_file = "assets/long.mp3"
-
-# optional volume 0 to 1.0
-volume = 0.8
-
-play_music(music_file, volume)
+subprocess.call(['afplay', "assets/longbeep.wav"])
